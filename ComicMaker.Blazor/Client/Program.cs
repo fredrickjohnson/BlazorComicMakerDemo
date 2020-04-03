87df96d1d02b44781f1a7c689f62c7332a0c5665
fredrickjohnson <fredrickjohnson@users.noreply.github.com>
2020-04-02 20:46:04 -0400

    Added Blazorize Bootstrap, Blazorize Font Awesome, and Blazorize Components, and Setup project to use blazorize

--- ComicMaker.Blazor/Client/Program.cs
+++ ComicMaker.Blazor/Client/Program.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Text;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -14,9 +17,21 @@ namespace ComicMaker.Blazor.Client
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("app");
 
+            builder.Services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true;
+            })
+            .AddBootstrapProviders()
+            .AddFontAwesomeIcons();
+
             builder.Services.AddBaseAddressHttpClient();
 
-            await builder.Build().RunAsync();
+            var host = builder.Build();
+            host.Services
+                .UseBootstrapProviders()
+                .UseFontAwesomeIcons();
+
+            await host.RunAsync();
         }
     }
 }

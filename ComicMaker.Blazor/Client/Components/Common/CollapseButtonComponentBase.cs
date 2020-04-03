3308e836b54b832e7f0676c1ddc95be118911d61
fredrickjohnson <fredrickjohnson@users.noreply.github.com>
2020-04-03 13:09:51 -0400

    Setup General Layout for Editor and Create Collapse Button to promote code reuse

---
+++ ComicMaker.Blazor/Client/Components/Common/CollapseButtonComponentBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace ComicMaker.Blazor.Client.Components.Common
+{
+    public class CollapseButtonComponentBase : ComponentBase
+    {
+        [Parameter]
+        public string Class { get; set; }
+
+        [Parameter]
+        public EventCallback Clicked { get; set; }
+
+        [Parameter]
+        public bool Toggle { get; set; }
+
+        protected void OnClick()
+        {
+            Clicked.InvokeAsync(Toggle);
+        }
+    }
+}
---
+++ ComicMaker.Blazor/Client/Components/Creator/EditorComponentBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace ComicMaker.Blazor.Client.Components.Creator
+{
+    public class EditorComponentBase : ComponentBase
+    {
+
+        public bool ShowImages { get; set; } = true;
+
+        public bool ShowAudio { get; set; } = false;
+    }
+}

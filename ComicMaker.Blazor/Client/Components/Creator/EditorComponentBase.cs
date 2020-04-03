8c3fb41a3b239c1fdd5d88d5bd33b6daeaf9bcb5
fredrickjohnson <fredrickjohnson@users.noreply.github.com>
2020-04-03 18:20:24 -0400

    Changed Editor Layout and added more buttons and right side control panel

--- ComicMaker.Blazor/Client/Components/Creator/EditorComponentBase.cs
+++ ComicMaker.Blazor/Client/Components/Creator/EditorComponentBase.cs
@@ -10,13 +10,34 @@ namespace ComicMaker.Blazor.Client.Components.Creator
     public class EditorComponentBase : ComponentBase
     {
         public TabService Tabs = new TabServiceFactory()
-            .AddTab("Assets","fas fa-photo-video")
-            .AddTab("Tests","fas fa-photo-video")
-            .AddTab("Dragons","fas fa-photo-video")
+            .AddTab("Home", "fas fa-home")
+            .AddTab("Variables","fas fa-times")
+            .AddTab("Props", "fas fa-couch")
+            .AddTab("Characters","fas fa-users")
+            .AddTab("Interactions", "fas fa-scroll")
             .Build();
             
         public Collapser ShowImages { get; set; } = new Collapser();
 
         public Collapser ShowAudio { get; set; } = new Collapser();
+
+        protected void Preview()
+        {
+
+        }
+
+        protected void Undo()
+        {
+
+        }
+
+        protected void Redo()
+        {
+
+        }
+
+        public bool EnableRedo { get; set; } = true;
+
+        public bool EnableUndo { get; set; } = true;
     }
 }

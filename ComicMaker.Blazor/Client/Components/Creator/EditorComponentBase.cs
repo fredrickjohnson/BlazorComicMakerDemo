bf16d5c5f7a4864a49b4a607348fe42c0c3e758e
fredrickjohnson <fredrickjohnson@users.noreply.github.com>
2020-04-04 00:10:00 -0400

    Experiment with Scene Sizes settled on 800 x 450 as standard

--- ComicMaker.Blazor/Client/Components/Creator/EditorComponentBase.cs
+++ ComicMaker.Blazor/Client/Components/Creator/EditorComponentBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
+using ComicMaker.Blazor.Client.Models;
 using ComicMaker.Blazor.Client.Services;
 using Microsoft.AspNetCore.Components;
 
@@ -10,16 +12,25 @@ namespace ComicMaker.Blazor.Client.Components.Creator
     public class EditorComponentBase : ComponentBase
     {
         public TabService Tabs = new TabServiceFactory()
-            .AddTab("Home", "fas fa-home")
-            .AddTab("Variables","fas fa-times")
-            .AddTab("Props", "fas fa-couch")
-            .AddTab("Characters","fas fa-users")
-            .AddTab("Interactions", "fas fa-scroll")
+            .AddTab("Scenes", "fas fa-home")
+            .AddTab("Backgrounds","fas fa-users")
+            .AddTab("Layouts", "fas fa-users")
+            .AddTab("Characters", "fas fa-users")
             .Build();
             
         public Collapser ShowImages { get; set; } = new Collapser();
 
         public Collapser ShowAudio { get; set; } = new Collapser();
+
+        public IList<string> Images { get; set; } = new List<string>
+        {
+            "",
+            ""
+        };
+
+        public Graphic Test { get; set; } = new Graphic();
+
+        public IList<string> Colors { get; set; } = new List<string>();
 
         protected void Preview()
         {
@@ -28,12 +39,20 @@ namespace ComicMaker.Blazor.Client.Components.Creator
 
         protected void Undo()
         {
+            this.Test.X += 10;
+            this.Test.Update();
+        }
 
+        protected void UTest()
+        {
+            Console.WriteLine("Hello World");
         }
 
         protected void Redo()
         {
-
+            this.Test.X -= 10;
+            this.Test.Update();
+            //this.Test.Xs = this.Test.X + "px";
         }
 
         public bool EnableRedo { get; set; } = true;
---
+++ ComicMaker.Blazor/Client/Models/Graphic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace ComicMaker.Blazor.Client.Models
+{
+    public class Graphic
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public string Xs { get; set; } = "10px";
+        public string Ys { get; set; } = "10px";
+
+        public string CSS { get; set; } = "width: 50px; height: 100px; position: relative; top: 10px; left: 10px;";
+
+        
+
+        public int Width { get; set; } = 100;
+        public int Height { get; set; } = 100;
+
+        public void Update()
+        {
+            Xs = X + "px";
+            Ys = Y + "px";
+        }
+    }
+}
